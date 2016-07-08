d96f1657884901a7594d9045c0af156c2b65b3fa
Swan Brun <swan2brun@gmail.com>
2016-07-08 16:34:06 +0200

    couche adherent(presque fini) + emprunt(debut)

---
+++ src/main/java/fr/iocean/application/model/adherent/AdherentRepository.java
@@ -0,0 +1,9 @@
+package fr.iocean.application.model.adherent;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdherentRepository extends JpaRepository<Adherent, Long> {
+
+}

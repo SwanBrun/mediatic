2cadaf33fe5c7fe975ba01a03b8a3d5d16178d57
Swan Brun <swan2brun@gmail.com>
2016-07-12 12:53:11 +0200

    redirection index.html

--- src/main/resources/public/app/js/login/loginModule.js
+++ src/main/resources/public/app/js/login/loginModule.js
@@ -3,9 +3,8 @@ angular.module('loginModule', ['ngRoute']);
 angular.module('loginModule').config(function($routeProvider) {
 
     $routeProvider.when('/', {
-    	//TODO : test à remettre quand ok
-        controller: 'AdherentsController',//LoginController
-        templateUrl: 'html/adherents/adherents.html'// 'html/login/login.html'
+        controller: 'LoginController',
+        templateUrl: 'html/login/login.html'
     });
 
 
